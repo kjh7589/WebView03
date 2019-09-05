8bc65911c4fa5e5ef662e67fd0dfb8cb9b2d1d8a
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-09-05 14:46:57 +0900

    0905

--- WebView03/ViewController.swift
+++ WebView03/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UISearchDisplayDelegate {
 
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let request = URLRequest(url: URL(string: "http://www.naver.com")!)
+        webView.load(request)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func backAction(_ sender: Any) {
+        if self.webView.canGoBack{
+            self.webView.goBack()
+        }
+    }
+    
+    @IBAction func forwardAction(_ sender: Any) {
+        if self.webView.canGoForward{
+            self.webView.goForward()
+        }
+    }
+    func searchBarSearchButtonClicked(_searchBar: UISearchBar){
+        let inputURL = searchBar.text!
+        let request = URLRequest(url:URL(string: inputURL)!)
+        webView.load(request)
+        self.view.endEditing(true)
+    }
 }
 
